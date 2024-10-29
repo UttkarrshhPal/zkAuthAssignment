7a5467fd84ebf5782af59dacd7ecb475b1d51a9c
UttkarrshhPal <palutkarsh1901@gmail.com>
2024-10-30 00:59:50 +0530

    removed material tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
 
-module.exports = withMT({
+module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-});
+};
